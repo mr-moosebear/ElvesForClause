b95bb0e7a67579575377ef8ce419310b97994e95
Mr-Moosebear <mr.moosebear86@gmail.com>
2024-11-20 14:07:42 -0500

    Player base Stats

--- elvesforclause/Characters/player_one.gd
+++ elvesforclause/Characters/player_one.gd
@@ -1,6 +1,13 @@
 extends CharacterBody2D
 
 
+const player_base_stats = {
+	"health": 5,
+	"speed": 100,
+	"power": 5,
+	"luck": 5
+}
+
 func _ready() -> void:
 	if !Global.short_range:
 		print("Hello")
