58fb1d26063b288ea6fb068ee0e12f6ad1e89f55
Mr-Moosebear <mr.moosebear86@gmail.com>
2024-11-22 19:05:40 -0500

    Detaching Nodes

--- elvesforclause/UI/inventory_ui.gd
+++ elvesforclause/UI/inventory_ui.gd
@@ -1,15 +1,1 @@
 extends Control
-
-var items : Dictionary = {
-	"wrapping_paper": "res://Art/MyArt/WrappingPaper.png",
-	"coal_gun": "res://Art/grenade_launcher.png"
-}
-
-func update_short_range() -> void:
-	if Global.short_range:
-		$ShortRangeSlot/Sprite2D.texture = load(items[Global.short_range])
-
-func update_long_range() -> void:
-	if Global.long_range:
-		print(Global.long_range)
-		$LongRangeSlot/Sprite2D.texture = load(items[Global.long_range])
